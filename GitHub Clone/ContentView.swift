d0b6c58b4008eb5904522bb501ec8e0b1d9ab5bf
rakan <Rakan.w10@outlook.com>
2023-10-24 10:23:55 +0300

    Initial Commit

---
+++ GitHub Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub Clone
+//
+//  Created by Rakan Alotaibi on 09/04/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub Clone/GitHub_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_CloneApp.swift
+//  GitHub Clone
+//
+//  Created by Rakan Alotaibi on 09/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
